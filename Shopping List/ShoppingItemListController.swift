9cb4c3d00203d7ffce302160497d3d6144ca102e
Troy Christian <apostasy8@gmail.com>
2019-11-15 11:58:20 -0500

    CustomCell layout and constraint in place

---
+++ Shopping List/ShoppingItemListController.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItemListController.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_219 on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+class ShoppingItemListController {
+    var shoppingItems: [ShoppingItem] = []
+    
+    var itemNames = [ShoppingItem(name:"Apple"),ShoppingItem(name:"Grapes"),ShoppingItem(name:"Milk"),ShoppingItem(name:"Muffin"),ShoppingItem(name:"Popcorn"),ShoppingItem(name:"Soda"),ShoppingItem(name:"Strawberrries")]
+}
