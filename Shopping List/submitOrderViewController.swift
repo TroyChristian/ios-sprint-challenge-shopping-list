4f8fff10b2b2646c0fcdff666eb171bc2b61bfe8
Troy Christian <apostasy8@gmail.com>
2019-11-16 16:50:36 -0500

    add SubmitOrderViewController

---
+++ Shopping List/submitOrderViewController.swift
@@ -0,0 +1,35 @@
+//
+//  submitOrderViewController.swift
+//  Shopping List
+//
+//  Created by Lambda_School_Loaner_219 on 11/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class submitOrderViewController: UIViewController {
+    @IBAction func submitOrder(_ sender: Any) {
+    }
+    @IBOutlet weak var notifLabel: UILabel!
+    @IBOutlet weak var addressTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
